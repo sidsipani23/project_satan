4e3c0f11ca460c1f7dac2e29407f2631cca07272
Siddhartha <siddhartha.sipani@leadsquared.com>
2022-02-05 20:42:11 +0530

    Dark mode issue fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import React, { useState, useMemo, createContext } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import CssBaseline from '@mui/material/CssBaseline';
 function MyApp({ Component, pageProps }) {
 	const [mode, setMode] = useState('light');
 	const colorMode = useMemo(
@@ -26,6 +26,7 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<ColorModeContext.Provider value={colorMode}>
 			<ThemeProvider theme={theme}>
+				<CssBaseline />
 				<Component {...pageProps} />
 			</ThemeProvider>
 		</ColorModeContext.Provider>
